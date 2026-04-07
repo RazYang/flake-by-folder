48ba07189828f5143787129e226b3605f07414db
RazYang <xzzorz@gmail.com>
2026-04-07 17:09:10 +0800

    refactor: lift mkIf guard to module level in devshells.nix

--- by-folder/devshells.nix
+++ by-folder/devshells.nix
@@ -1,33 +1,35 @@
-{ lib, config, inputs, ... }:
+{
+  lib,
+  config,
+  ...
+}:
 let
   cfg = config.flake-by-folder;
   devshellsDir = cfg.root + "/devshells";
 in
-{
+lib.mkIf (cfg.devshells.enable && builtins.pathExists devshellsDir) {
   perSystem =
     { config, pkgs, ... }:
     let
       starshipConfig = pkgs.writers.writeTOML "starship.toml" { };
     in
     {
-      devshells = lib.mkIf (cfg.devshells.enable && builtins.pathExists devshellsDir) (
-        lib.pipe (lib.fileset.fileFilter ({ name, ... }: name == "devshell.nix") devshellsDir) [
-          (lib.fileset.toList)
-          (lib.map (path: {
-            name = builtins.baseNameOf (builtins.dirOf path);
-            value = {
-              imports = [ (import path config.allModuleArgs) ];
-              devshell = {
-                motd = lib.mkDefault "";
-                interactive.PS1.text = ''
-                  export STARSHIP_CONFIG=${starshipConfig}
-                  eval -- "''$(${pkgs.starship}/bin/starship init bash --print-full-init)"
-                '';
-              };
+      devshells = lib.pipe (lib.fileset.fileFilter ({ name, ... }: name == "devshell.nix") devshellsDir) [
+        (lib.fileset.toList)
+        (lib.map (path: {
+          name = builtins.baseNameOf (builtins.dirOf path);
+          value = {
+            imports = [ (import path config.allModuleArgs) ];
+            devshell = {
+              motd = lib.mkDefault "";
+              interactive.PS1.text = ''
+                export STARSHIP_CONFIG=${starshipConfig}
+                eval -- "''$(${pkgs.starship}/bin/starship init bash --print-full-init)"
+              '';
             };
-          }))
-          (lib.listToAttrs)
-        ]
-      );
+          };
+        }))
+        (lib.listToAttrs)
+      ];
     };
 }
